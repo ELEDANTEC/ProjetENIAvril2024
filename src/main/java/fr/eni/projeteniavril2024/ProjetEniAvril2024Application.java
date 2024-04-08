fc6d89e2ff10eac669e09d348ae3b5947c213630
Yoann JEAUNEAU <yoann.jeauneau2023@campus-eni.fr>
2024-04-08 12:07:55 +0200

    first-commit

---
+++ src/main/java/fr/eni/projeteniavril2024/ProjetEniAvril2024Application.java
@@ -0,0 +1,13 @@
+package fr.eni.projeteniavril2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetEniAvril2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetEniAvril2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/projeteniavril2024/ProjetEniAvril2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.projeteniavril2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetEniAvril2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
