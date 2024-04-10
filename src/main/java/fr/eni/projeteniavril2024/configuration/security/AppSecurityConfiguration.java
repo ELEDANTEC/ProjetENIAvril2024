b0f30ebba57fe708b2cba5df9c2529f8c1e0d851
Evariste <evariste@ledantec.bzh>
2024-04-10 15:52:09 +0200

    [ADD] connexion to db

--- src/main/java/fr/eni/projeteniavril2024/configuration/security/AppSecurityConfiguration.java
+++ src/main/java/fr/eni/projeteniavril2024/configuration/security/AppSecurityConfiguration.java
@@ -2,19 +2,56 @@ package fr.eni.projeteniavril2024.configuration.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfiguration {
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
             auth.requestMatchers("/*").permitAll();
+            auth.requestMatchers(HttpMethod.GET, "/*").hasRole("ADMIN");
+            auth.requestMatchers(HttpMethod.POST, "/*").hasRole("ADMIN");
+            auth.requestMatchers(HttpMethod.GET, "/*").hasRole("MEMBER");
+            auth.requestMatchers(HttpMethod.POST, "/*").hasRole("MEMBER");
         });
 
+        http.formLogin(form -> {
+            form.loginPage("/login").permitAll();
+            form.defaultSuccessUrl("/test");
+        });
+
+        http.logout(logout ->
+                logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID")
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+        );
+
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        String sqlUsername = "SELECT username, password, 1 FROM USERS WHERE username = ?;";
+        String sqlAuthorities = "SELECT username, administrator FROM USERS WHERE username = ?";
+        jdbcUserDetailsManager.setUsersByUsernameQuery(sqlUsername);
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(sqlAuthorities);
+
+        return jdbcUserDetailsManager;
     }
 }
